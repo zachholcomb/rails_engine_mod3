8545de0f2709c5f076b14dbc780ca36d46457455
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-16 14:48:05 -0600

    Add item to merchants test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe 'relationships' do
-    it { should belong_to(:merchant) }
+    it { should belong_to :merchant }
   end
 end
