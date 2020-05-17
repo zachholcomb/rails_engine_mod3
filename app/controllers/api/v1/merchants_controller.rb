48bddfaf438f6e6ed8c26e1925d8835aa579bce7
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-16 18:06:46 -0600

    Add merchants controller with enpoints and matching routes

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,28 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+  def create
+    render json: MerchantSerializer.new(Merchant.create(merchant_params))
+  end
+
+  def update
+    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
+  end
+
+  def destroy
+    merchant = Merchant.find(params[:id])
+    Merchant.delete(params[:id])
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
