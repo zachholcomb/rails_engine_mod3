e94e4276fc590e17fd8a4180e72d7d00da1bac27
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-17 22:56:48 -0600

    Add routes for merchant searches

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show, :create, :update, :destroy] 
+
+      namespace :merchants do
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+      end
+
+      resources :merchants, only: [:index, :show, :create, :update, :destroy] do
+        resources :items, only: [:index]
+      end
+
       resources :items, only: [:index, :show, :create, :update, :destroy]
+
+      get '/items/:item_id/merchant', to: 'merchants#show'
     end
   end
 end
