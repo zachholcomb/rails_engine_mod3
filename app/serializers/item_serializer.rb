669df29b546b7cffce1575c52c33f878a9bfef28
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-17 22:57:06 -0600

    Add relationships to serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,5 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :description, :unit_price, :merchant_id
+  belongs_to :merchant
 end
--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,4 +1,6 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
+
+  has_many :items
 end
