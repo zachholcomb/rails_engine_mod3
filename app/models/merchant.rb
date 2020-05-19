3a9df68122264646965f0a00088eb07ae1975837
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-19 00:29:13 -0600

    Add transaction relationship and most revenue model method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,18 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :item_invoices, through: :invoices
+  has_many :transactions, through: :invoices
   scope :filter_by_name, -> (name) { where("lower(name) like ?", "%#{name.downcase}%") }
   scope :filter_by_created_at, -> (created_at) { where("Date(created_at) = ?", "#{created_at}") }
   scope :filter_by_updated_at, -> (updated_at) { where("Date(updated_at) = ?", "#{updated_at}") }
+
+  def self.most_revenue(limit_quantity)
+    Merchant.joins(:invoices, :item_invoices, :transactions)
+            .where('transactions.result = 0')
+            .select('merchants.*, sum(item_invoices.unit_price * item_invoices.quantity) as revenue')
+            .group(:id)
+            .order('revenue desc')
+            .limit(limit_quantity)
+  end
 end
