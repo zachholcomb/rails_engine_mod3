1fe48e948c777050f026a2bb056ec99e9623a0c2
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-21 12:45:47 -0600

    Updates routes to new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
       resources :items, execpt: [:new, :edit]
 
       get '/revenue', to: 'revenue#index'
-      get '/merchants/:id/revenue', to: 'revenue#show'
+      get '/merchants/:id/revenue', to: 'merchant_revenue#show'
     end
   end
 end
