2305c90191a38418cb1eead9bd4fb048c4dba9c6
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-21 10:23:48 -0600

    Update actions in controllers to follow srp

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    if params[:merchant_id]
-      render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
-    else
-      render json: ItemSerializer.new(Item.all)
-    end
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
