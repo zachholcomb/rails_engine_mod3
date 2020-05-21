94373341b3df8ea1adc7545310a0d2232c59962b
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-21 10:23:06 -0600

    Add separate controller to handle relationships

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
-    merchants = Merchant.most_items(params[:quantity])
-    render json: MerchantSerializer.new(merchants)
+    render json: ItemSerializer.new(Merchant.find(params[:id]).items)
   end
 end
