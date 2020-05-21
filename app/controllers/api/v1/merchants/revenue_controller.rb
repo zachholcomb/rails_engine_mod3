6dfacbcfe525be335c0d8242b187eb95ba0b23b0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-21 10:23:17 -0600

    Remove old controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::Merchants::RevenueController < ApplicationController
-  def index
-    merchants = Merchant.most_revenue(params[:quantity])
-    render json: MerchantSerializer.new(merchants)
-  end
-end
