40a5f672dec86fa99db7910b2d74a59da5b4879f
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-21 12:44:28 -0600

    Reorganizes revenue controllers for more restful solution

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::RevenueController < ApplicationController
   def index
-    @revenue = Merchant.revenue_date_range(params[:start], params[:end])
-    render '/revenue/index.json'
-  end
-
-  def show
-    @revenue = Merchant.revenue(params[:id])
-    render  '/revenue/show.json'
+    @value = Merchant.revenue_date_range(params[:start], params[:end])
+    render json: RevenueSerializer.new(Revenue.new(@value))
   end
 end
