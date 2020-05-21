c28a591488ff659acb1656ca54cc3b1de9297ec0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-21 10:24:44 -0600

    Switched relationship routes to namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-
       namespace :merchants do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
-        get '/most_revenue', to: 'revenue#index'
-        get '/most_items', to: 'items#index'
+        get '/most_revenue', to: 'most_revenue#index'
+        get '/most_items', to: 'most_items#index'
+        get '/:id/items', to: 'items#index'
       end
+      resources :merchants, except: [:new, :edit] 
 
       namespace :items do
+        get '/:id/merchant', to: 'merchant#show'
         get '/find', to: 'search#show'
         get 'find_all', to: 'search#index'
       end
-
-      resources :merchants, only: [:index, :show, :create, :update, :destroy] do
-        resources :items, only: [:index]
-      end
-
-      resources :items, only: [:index, :show, :create, :update, :destroy]
-
-      get '/items/:item_id/merchant', to: 'merchants#show'
+      resources :items, execpt: [:new, :edit]
 
       get '/revenue', to: 'revenue#index'
       get '/merchants/:id/revenue', to: 'revenue#show'
